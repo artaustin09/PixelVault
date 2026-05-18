a2b96f7aee06ed6187f2b3d5c21d75b8423d5248
CARLOS-URIEL-AVALOS-GARCIA <avalosambrocio2@gmail.com>
2026-05-17 19:46:57 -0700

    carlos uriel agregue el codigo de admin.py en base a los codigos ya agregados por mis compañeros

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Desarrolladora, Plataforma, Vendedor, Videojuego, Cliente, Venta
 
-# Register your models here.
+admin.site.register(Desarrolladora)
+admin.site.register(Plataforma)
+admin.site.register(Vendedor)
+admin.site.register(Videojuego)
+admin.site.register(Cliente)
+admin.site.register(Venta)
