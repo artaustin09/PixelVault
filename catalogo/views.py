150ff5b3aebdf1d5b50674af5cd90c2d962af90f
elena333g <elenabalbuena333@gmail.com>
2026-05-17 19:22:59 -0700

    Luz elena implemente el codigo en el archivo views.py

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Videojuego
 
-# Create your views here.
+def home(request):
+    busqueda = request.GET.get('buscar')
+    if busqueda:
+        juegos = Videojuego.objects.filter(titulo__icontains=busqueda)
+    else:
+        juegos = Videojuego.objects.all()
+    return render(request, 'catalogo/index.html', {'juegos': juegos, 'busqueda': busqueda})
+
+def detalle(request, juego_id):
+    juego = get_object_or_404(Videojuego, pk=juego_id)
+    return render(request, 'catalogo/detalle_juego.html', {'juego': juego})
