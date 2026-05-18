38581a444f98a7b7a7d4dcd0935faf41d5cb2431
artaustin09 <a.r.t.austin09@gmail.com>
2026-05-17 18:29:36 -0700

    Austin: Configuración de settings.py terminada

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'catalogo'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Para las portadas de los videojuegos
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
