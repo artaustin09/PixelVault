304ca350b843b78bcbfcfc116f47d707803fed10
juanjael2009-png <Juanjael2009@gmail.com>
2026-05-17 19:27:53 -0700

    juan martinez implemente el codigo en models.py

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Desarrolladora(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.CharField(max_length=50)
+    def __str__(self): return self.nombre
+
+class Plataforma(models.Model):
+    nombre = models.CharField(max_length=50) # Ej: PS5, Xbox, PC
+    fabricante = models.CharField(max_length=50)
+    def __str__(self): return self.nombre
+
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    alias = models.CharField(max_length=50) # Gamertag del vendedor
+    def __str__(self): return self.nombre
