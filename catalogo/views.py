103194fda980bd6c3ef68497dcca857cbb241e03
artaustin09 <a.r.t.austin09>
2026-05-25 17:02:36 -0700

    PIXELVAULT: Navegación y detalle de juegos funcional

--- catalogo/views.py
+++ catalogo/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Videojuego
 
 def home(request):
-    busqueda = request.GET.get('buscar')
-    if busqueda:
-        juegos = Videojuego.objects.filter(titulo__icontains=busqueda)
-    else:
-        juegos = Videojuego.objects.all()
-    return render(request, 'catalogo/index.html', {'juegos': juegos, 'busqueda': busqueda})
+    juegos = Videojuego.objects.all()
+    # Buscador opcional
+    query = request.GET.get('q')
+    if query:
+        juegos = juegos.filter(titulo__icontains=query)
+    return render(request, 'catalogo/index.html', {'juegos': juegos})
 
 def detalle(request, juego_id):
+    # Trae el juego o lanza error 404 si el ID no existe
     juego = get_object_or_404(Videojuego, pk=juego_id)
     return render(request, 'catalogo/detalle_juego.html', {'juego': juego})
