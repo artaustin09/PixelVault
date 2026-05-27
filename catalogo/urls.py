43bcdaf45c45dadc1d929a1f604fa7f91d40e3e0
artaustin09 <a.r.t.austin09@gmail.com>
2026-05-27 12:18:52 -0700

    problemas arreglados todo funcionando

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views # Importante para Login/Logout
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # Cambiamos int por uuid para que funcione con tus modelos
-    path('juego/<uuid:juego_id>/', views.detalle_juego, name='detalle_juego'),
     
-    # Rutas del Carrito (También con uuid)
+    # Detalle del juego (Cambiado de uuid a int)
+    path('juego/<int:juego_id>/', views.detalle_juego, name='detalle_juego'),
+
+    # Rutas del Carrito (Todas con <int:juego_id>)
     path('carrito/', views.ver_carrito, name='ver_carrito'),
-    path('agregar/<uuid:juego_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
-    path('eliminar/<uuid:juego_id>/', views.eliminar_del_carrito, name='eliminar_del_carrito'),
+    path('agregar/<int:juego_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
+    path('eliminar/<int:juego_id>/', views.eliminar_del_carrito, name='eliminar_del_carrito'),
     path('limpiar/', views.limpiar_carrito, name='limpiar_carrito'),
     path('confirmar-compra/', views.confirmar_compra, name='confirmar_compra'),
 
